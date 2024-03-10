5f305e9a7b5f4912315d0c756106aaced49df11e
Stefan Chiper <stefan814759@gmail.com>
2024-03-10 18:58:49 +0200

    tema1

--- lab2/lab2/lab_dut/instr_register.sv
+++ lab2/lab2/lab_dut/instr_register.sv
@@ -1,48 +1,54 @@
-/***********************************************************************
- * A SystemVerilog RTL model of an instruction regisgter
- *
- * An error can be injected into the design by invoking compilation with
- * the option:  +define+FORCE_LOAD_ERROR
- *
- **********************************************************************/
+	/***********************************************************************
+	 * A SystemVerilog RTL model of an instruction regisgter
+	 *
+	 * An error can be injected into the design by invoking compilation with
+	 * the option:  +define+FORCE_LOAD_ERROR
+	 *
+	 **********************************************************************/
 
-module instr_register
-import instr_register_pkg::*;  // user-defined types are defined in instr_register_pkg.sv
-(input  logic          clk,
- input  logic          load_en,
- input  logic          reset_n,
- input  operand_t      operand_a,
- input  operand_t      operand_b,
- input  opcode_t       opcode,
- input  address_t      write_pointer,
- input  address_t      read_pointer,
- output instruction_t  instruction_word
-);
-  timeunit 1ns/1ns;
+	module instr_register
+	import instr_register_pkg::*;  // user-defined types are defined in instr_register_pkg.sv
+	(input  logic          clk,
+	 input  logic          load_en,
+	 input  logic          reset_n,
+	 input  operand_t      operand_a,
+	 input  operand_t      operand_b,
+	 input  opcode_t       opcode,
+	 input  address_t      write_pointer,
+	 input  address_t      read_pointer,
+	 output instruction_t  instruction_word
+  ); 
+	  timeunit 1ns/1ns;
 
-  instruction_t  iw_reg [0:31];  // an array of instruction_word structures
+	  instruction_t  iw_reg [0:31];  // an array of instruction_word structures
 
-  // write to the register
-  always@(posedge clk, negedge reset_n)   // write into register
-    if (!reset_n) begin
-      foreach (iw_reg[i])
-        iw_reg[i] = '{opc:ZERO,default:0};  // reset to all zeros
-    end// ' se refere ca e indiferent de numarul de biti
-    else if (load_en) begin//daca am loand_en in incarc valorile a si b in array
-    //trebuie pus un switch care daca opcode e add facem adunare si le stocam intr-o variabila noua , trebuie sa vedem pe cati biti e
-      iw_reg[write_pointer] = '{opcode,operand_a,operand_b};//poate sa fie orice valorea, deoarece se suprascrie
-    end
+	  // write to the register
+	  always@(posedge clk, negedge reset_n)   // write into register 
+		if (!reset_n) begin
+		  foreach (iw_reg[i])
+			iw_reg[i] = '{opc:ZERO,default:0};  // reset to all zeros 
+		end
+		else if (load_en) begin 
+		  case (opcode)
+			ZERO: iw_reg[write_pointer] = '{opcode,operand_a,operand_b, 0}; //rezultatul este setat la 0
+			PASSA: iw_reg[write_pointer] = '{opcode,operand_a,operand_b, operand_a}; //rezultatul este operandul A
+			PASSB: iw_reg[write_pointer] = '{opcode,operand_a,operand_b, operand_b};//rezultatul este operandul B
+			ADD: iw_reg[write_pointer] = '{opcode,operand_a,operand_b, operand_a + operand_b};
+			SUB: iw_reg[write_pointer] = '{opcode,operand_a,operand_b, operand_a - operand_b};
+			MULT: iw_reg[write_pointer] = '{opcode,operand_a,operand_b, operand_a * operand_b};
+			DIV: iw_reg[write_pointer] = '{opcode,operand_a,operand_b, operand_a / operand_b};
+			MOD: iw_reg[write_pointer] = '{opcode,operand_a,operand_b, operand_a % operand_b};  
+		  endcase 
+		end
 
-//letch comuta pe palier si bisatbilu pe front 
-  // read from the register
- 
- assign instruction_word = iw_reg[read_pointer];  // continuously read from register
-//read_pointer ii spune ca citesc ce  in structura, daca nu am intializat nicio structra citesc 0 , iar inainte de reste citesc x-uri
-// compile with +define+FORCE_LOAD_ERROR to inject a functional bug for verification to catch
-`ifdef FORCE_LOAD_ERROR
-initial begin
-  force operand_b = operand_a; // cause wrong value to be loaded into operand_b
-end
-`endif
+	  // read from the register
+	  assign instruction_word = iw_reg[read_pointer];  // continuously read from register
 
-endmodule: instr_register
+	// compile with +define+FORCE_LOAD_ERROR to inject a functional bug for verification to catch
+	`ifdef FORCE_LOAD_ERROR
+	initial begin
+	  force operand_b = operand_a; // cause wrong value to be loaded into operand_b
+	end
+	`endif
+
+	endmodule: instr_register
--- lab2/lab2/lab_dut/instr_register_pkg.sv
+++ lab2/lab2/lab_dut/instr_register_pkg.sv
@@ -5,9 +5,9 @@
 package instr_register_pkg;
   timeunit 1ns/1ns;
 
-  typedef enum logic [3:0] {
-  	ZERO,
-    PASSA,
+  typedef enum logic [3:0] {   
+  	ZERO, 
+    PASSA, 
     PASSB,
     ADD,
     SUB,
@@ -15,15 +15,20 @@ package instr_register_pkg;
     DIV,
     MOD
   } opcode_t;
-
-  typedef logic signed [31:0] operand_t;
   
-  typedef logic [4:0] address_t;
+  
+ typedef logic signed [31:0] operand_t;   
+
+  typedef logic signed [63:0] result_t; 
+  
+  typedef logic [4:0] address_t;     
   
   typedef struct {
     opcode_t  opc;
     operand_t op_a;
-    operand_t op_b;
+    operand_t op_b; 
+    result_t rezultat; 
   } instruction_t;
-
+  
+  
 endpackage: instr_register_pkg
